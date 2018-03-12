b77f83f9889def13884a1835d915fa2db2ef1c8e
Md Kamrul hasan <mkhsojib@gmail.com>
2018-03-12 12:07:20 +0600

    home page setup

--- wphierarchy/index.php
+++ wphierarchy/index.php
@@ -1,5 +1,36 @@
 <?php get_header(); ?>
-    <h1>index.php</h1>
+
+<div id="primary" class="content-area">
+
+    <main id="main" class="site-main" role="main">
+
+      <article id="post-<?php the_id(); ?>" <?php post_class(); ?>>
+
+
+      <header class="entry-header">
+
+          <h1>index.php</h1>
+
+
+
+
+      </header>
+
+      <div class="entry-content">
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      </div>
+
+      </article>
+
+
+
+    </main>
+
+
+</div>
+
 
 
 
